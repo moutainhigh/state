5b030d5a46f431121bf2b461aa9565b1059f18a7
chaoyer <liwc@shinemo.com>
2019-06-11 12:00:55 +0800

    redis

--- score-core/src/main/java/com/shinemo/score/core/configuration/ProviderConfiguration.java
+++ score-core/src/main/java/com/shinemo/score/core/configuration/ProviderConfiguration.java
@@ -1,9 +1,13 @@
 package com.shinemo.score.core.configuration;
 
+import com.shinemo.my.redis.domain.RedisSentinelNode;
+import com.shinemo.my.redis.service.RedisService;
+import com.shinemo.my.redis.service.impl.RedisServiceImpl;
 import com.shinemo.score.client.comment.facade.CommentFacadeService;
 import com.shinemo.score.client.user.facade.UserInfoFacadeService;
 import com.shinemo.jce.spring.AaceProviderBean;
 import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.DependsOn;
@@ -25,7 +29,7 @@ public class ProviderConfiguration {
      * @author zhangyan
      * @date 2018-05-29
      **/
-    @Bean(initMethod="init")
+    @Bean(initMethod = "init")
     @DependsOn("userInfoFacadeService")
     public AaceProviderBean providerUserInfoFacadeService(@Qualifier("userInfoFacadeService") UserInfoFacadeService userInfoFacadeService) {
         AaceProviderBean aaceProviderBean = new AaceProviderBean();
@@ -35,12 +39,23 @@ public class ProviderConfiguration {
         return aaceProviderBean;
     }
 
-    @Bean(initMethod="init")
+    @Bean(initMethod = "init")
     @DependsOn("commentFacadeService")
     public AaceProviderBean providerCommentFacadeService(@Qualifier("commentFacadeService") CommentFacadeService commentFacadeService) {
         AaceProviderBean aaceProviderBean = new AaceProviderBean();
         aaceProviderBean.setInterfaceName(CommentFacadeService.class.getName());
         aaceProviderBean.setTarget(commentFacadeService);
         return aaceProviderBean;
+    }
+
+    @Bean
+    public RedisService redisService(@Value("${spring.redis.database}") Integer database,
+                                     @Value("${spring.redis.ip}") String redisIp,
+                                     @Value("${spring.redis.master.name}") String masterName,
+                                     @Value("${spring.redis.pwd}") String pwd) {
+        RedisSentinelNode node = new RedisSentinelNode(redisIp, masterName);
+        node.setDatabase(database);
+        node.setPassword(pwd);
+        return new RedisServiceImpl(node);
     }
 }
