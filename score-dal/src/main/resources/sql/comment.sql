b69bb0aecd868dddd8063e5a943fa36a8576dbac
chaoyer <liwc@shinemo.com>
2019-06-13 15:26:09 +0800

    sql

--- score-dal/src/main/resources/sql/comment.sql
+++ score-dal/src/main/resources/sql/comment.sql
@@ -1,28 +1,27 @@
-DROP TABLE IF EXISTS `comment`;
 CREATE TABLE `comment`
 (
-  `id`            bigint       NOT NULL AUTO_INCREMENT,
-  `gmt_create`    datetime     NOT NULL,
-  `gmt_modified`  datetime     NOT NULL,
-  `status`        int          not null default '1' COMMENT '删除状态 1-正常 0-删除',
-  `video_id`      varchar(128) not null comment '外部电影id',
-  `video_type`    int          NOT NULL COMMENT '视频类型',
-  `content`       varchar(255) NOT NULL COMMENT '评论内容',
-  `like_num`      bigint       NOT NULL DEFAULT '0' COMMENT '赞数量',
-  `reply_num`     int       NOT NULL DEFAULT '0' COMMENT '回复数量',
-  `uid`           bigint       not null comment '用户id',
-  `mobile`        varchar(30)  not null comment '手机号码',
-  `name`          varchar(256)          DEFAULT null comment '用户名称',
-  `avatar_url`    varchar(256)          DEFAULT null comment '头像',
-  `device`        varchar(80)           DEFAULT null comment '设备',
-  `net_type`      varchar(80)           DEFAULT null comment '网络类型',
-  `sort`          int                   default '0' comment '排序权重',
-  `extend`        text                  DEFAULT null comment '扩展字段',
-  `history_reply` text                  DEFAULT null comment '最近的回复(最近3条)',
-  `version`       bigint       not null default '0' COMMENT '版本号',
+  `id`            bigint(20)          NOT NULL AUTO_INCREMENT,
+  `gmt_create`    datetime            NOT NULL,
+  `gmt_modified`  datetime            NOT NULL,
+  `status`        int(11)             NOT NULL DEFAULT '1' COMMENT '删除状态 1-正常 0-删除',
+  `video_id`      varchar(128)        NOT NULL DEFAULT '' COMMENT '外部视频id',
+  `video_type`    int(11)             NOT NULL COMMENT '视频类型',
+  `content`       blob                NOT NULL COMMENT '评论内容',
+  `like_num`      bigint(20)          NOT NULL DEFAULT '0' COMMENT '赞数量',
+  `uid`           bigint(20) unsigned NOT NULL COMMENT '用户id',
+  `mobile`        varchar(30)         NOT NULL DEFAULT '' COMMENT '手机号码',
+  `name`          varchar(256)                 DEFAULT NULL COMMENT '用户名称',
+  `avatar_url`    varchar(256)                 DEFAULT NULL COMMENT '头像',
+  `device`        varchar(80)                  DEFAULT NULL COMMENT '设备',
+  `net_type`      varchar(80)                  DEFAULT NULL COMMENT '网络类型',
+  `sort`          int(11)                      DEFAULT '0' COMMENT '排序权重',
+  `extend`        text COMMENT '扩展字段',
+  `history_reply` text COMMENT '最近的回复(最近3条)',
+  `reply_num`     int(11)             NOT NULL DEFAULT '0' COMMENT '回复的数量',
+  `version`       bigint(20)          NOT NULL DEFAULT '0' COMMENT '版本号',
   PRIMARY KEY (`id`),
   KEY `idx_u` (`uid`) USING BTREE,
   KEY `idx_v_t` (`video_id`, `video_type`) USING BTREE
 ) ENGINE = InnoDB
-  AUTO_INCREMENT = 1
+  AUTO_INCREMENT = 15
   DEFAULT CHARSET = utf8 COMMENT ='评论表';
