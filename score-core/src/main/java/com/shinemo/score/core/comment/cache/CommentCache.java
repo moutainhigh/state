a44f15fd583fba6891414a539b513f129c00295a
chaoyer <liwc@shinemo.com>
2019-08-15 11:26:57 +0800

    fix bug

--- score-core/src/main/java/com/shinemo/score/core/comment/cache/CommentCache.java
+++ score-core/src/main/java/com/shinemo/score/core/comment/cache/CommentCache.java
@@ -1,8 +1,5 @@
 package com.shinemo.score.core.comment.cache;
 
-import com.google.common.cache.CacheBuilder;
-import com.google.common.cache.CacheLoader;
-import com.google.common.cache.LoadingCache;
 import com.shinemo.my.redis.service.RedisService;
 import com.shinemo.score.client.comment.domain.CommentDO;
 import com.shinemo.score.core.comment.service.CommentService;
@@ -10,7 +7,6 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Component;
 
 import javax.annotation.Resource;
-import java.util.concurrent.TimeUnit;
 
 /**
  * @author wenchao.li
@@ -31,26 +27,19 @@ public class CommentCache {
     // comment redis key expired time 1天
     private final static Integer COMMENT_KEY_EXPIRE = 60 * 60 * 24;
 
-    private final LoadingCache<Long, CommentDO> cache = CacheBuilder.newBuilder()
-            .expireAfterWrite(5, TimeUnit.SECONDS).initialCapacity(100)
-            .build(
-                    new CacheLoader<Long, CommentDO>() {
-                        public CommentDO load(Long key) {
-                            // 先走redis
-                            CommentDO comment = redisService.get(keyFormat(key), CommentDO.class);
-                            if (comment != null) {
-                                return comment;
-                            }
-                            // 最终走db
-                            return commentService.getByIdFromDB(key);
-                        }
-                    });
-
 
     public CommentDO get(Long key) {
         try {
-            // 没get到，会走guava loader
-            return cache.get(key);
+            // 先走redis
+            CommentDO comment = redisService.get(keyFormat(key), CommentDO.class);
+            if (comment != null) {
+                return comment;
+            }
+            // 最终走db
+            comment = commentService.getByIdFromDB(key);
+            // 加入缓存
+            put(comment);
+            return comment;
         } catch (Exception e) {
             log.error("[commentCache] get cache has ex", e);
             return null;
@@ -58,16 +47,12 @@ public class CommentCache {
     }
 
     public void put(CommentDO commentDO) {
-
         redisService.set(keyFormat(commentDO.getId()), commentDO, COMMENT_KEY_EXPIRE);
-        cache.put(commentDO.getId(), commentDO);
     }
 
     // 清除key
     private void remove(Long key) {
-
         redisService.del(keyFormat(key));
-        cache.invalidate(key);
     }
 
     // 刷新缓存
