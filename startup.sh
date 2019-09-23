480867ca11f7eb996a100a7d202d4bb353b459f6
zhangyan <578033593@qq.com>
2019-09-23 14:08:06 +0800

    fix

--- startup.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-echo -e "start .."
-source /etc/profile
-nohup java -server -Xms1000m -Xmx1000m -Xrunjdwp:transport=dt_socket,server=y,suspend=n,address=7184  -Dapp_name=score -jar /home/admin/software/score/score.jar 1>>/home/admin/software/score/score.log 2>&1  &
-
-echo "start score success..."
